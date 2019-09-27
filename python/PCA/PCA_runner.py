corrected scaling for multisite simulation

python/PCA/PCA_runner.py
@@ -12,11 +12,11 @@ from import_data import CustomDataImporter
 
 class SimulationRunner():
     def __init__(self):
-        #spec = importlib.util.spec_from_file_location("module.name","../../import_export/import_data.py")
-        #foo = importlib.util.module_from_spec(spec)
-        #spec.loader.exec_module(foo)
+        spec = importlib.util.spec_from_file_location("module.name","../../import_export/import_data.py")
+        foo = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(foo)
         self.ddppca = Distributed_DP_PCA()
-        self.importer = CustomDataImporter()
+        self.importer = foo.CustomDataImporter()
 
 
     def create_annotation(self, n, split, i, epsilon, delta):
@@ -74,7 +74,7 @@ class SimulationRunner():
                             #vec = self.ddppca.normalize_eigenspaces([v1, vec])[1]
 
                         # projection matrix for sanity check.
-                        #TODO check
+                        #TODO check probably wrong
                         proj = sc.dot( vec[:, 0:dm],np.diag(val[0:dm]))
                         proj = np.concatenate((proj, self.create_annotation(proj.shape[0], split, i, epsilon, delta)),
                                               axis=1)
@@ -180,7 +180,7 @@ class SimulationRunner():
 
 if __name__=="__main__":
     print('run')
-    '''
+
     parser = ap.ArgumentParser(description='Run distributed PCA simulation')
     parser.add_argument('-f', metavar='file', type=str, help='filename of data file; file should be tab separated')
     parser.add_argument('-d', metavar='dimensions', type=int, help='number of principal components to return')
@@ -251,11 +251,11 @@ if __name__=="__main__":
 
     #simulation.run_standalone(datafile, outfile=outfile, dims=dimensions, header=header,rownames=rownames,center = center, scale_var = scale_var, scale01 = scale01, scale_unit=scale_unit, transpose = True)
 
-    '''
+
     simulation = SimulationRunner()
 
-    simulation.run_multiple_simulations(datafile='/home/anne/Documents/featurecloud/results/simulation_breast/data_real.tsv', dims=10, noise=False, splits=3, repeat = 1, epsilons=[0.01], deltas=[0.01],dirname='/home/anne/Documents/featurecloud/results/tesy/', save_eigen=False, transpose = False, center = True, scale_var = True, scale01 = False, scale_unit=True)
-    '''
+    #simulation.run_multiple_simulations(datafile='/home/anne/Documents/featurecloud/results/simulation_breast/data_real.tsv', dims=10, noise=False, splits=3, repeat = 1, epsilons=[0.01], deltas=[0.01],dirname='/home/anne/Documents/featurecloud/results/tesy/', save_eigen=False, transpose = False, center = True, scale_var = True, scale01 = False, scale_unit=True)
+
     if args.A:
         simulation.run_standalone(args.f, outfile=args.p, dims=args.d, header=header, rownames=rownames,
                               center=args.t, scale_var=args.v, scale01=args.z, scale_unit=args.u,
@@ -271,7 +271,6 @@ if __name__=="__main__":
                                         epsilons=epsilons, deltas=deltas, dirname=args.p, save_eigen=args.s,
                                         transpose=False, center=args.t, scale_var=args.v, scale01=args.z,
                                         scale_unit=args.u, noise=True, splits=args.k)
-    
-    '''
+
 
 