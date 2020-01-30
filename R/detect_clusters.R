4bece96b782031ac74c5bc160b4648f15450c0f1
Anne Hartebrodt <anne.hartebrodt@tum.de>
2020-01-30 12:28:41 +0100

    added cluster detection script

--- R/detect_clusters.R
+++ R/detect_clusters.R
@@ -4,3 +4,7 @@ source('PCA_bor_aor_plot.R')
 
 #Extract clusters with k-means
 clusters<-kmeans(pca$x[,1:2], 2)
+p.out<-ggbiplot(pca.outlier.free, var.axes = F, labels = lab, groups = as.factor(clusters$cluster))
+p.out
+
+cluster_result<-data.table(sample_id = 1:nrow(outlier.free), cluster_id = clusters$cluster)
