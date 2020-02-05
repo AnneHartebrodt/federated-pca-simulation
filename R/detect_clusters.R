b13fa1ea00408392a95c66c5f39a6f7a6c9a56cd
Anne Hartebrodt <anne.hartebrodt@tum.de>
2020-02-05 16:51:48 +0100

    cluster splits

--- R/detect_clusters.R
+++ R/detect_clusters.R
@@ -3,15 +3,19 @@ source('PCA_bor_aor_plot.R')
 
 
 #Extract clusters with k-means
-clusters<-kmeans(pca.outlier.free$x[,1:2], centers = opt$k, iter = 10000)
-p.out.clu<-ggbiplot(pca.outlier.free, var.axes = F, labels = lab, groups = as.factor(clusters$cluster))
+cluster_split<-function(x){
+  clusters<-kmeans(pca.outlier.free$x[,c(1,x)], centers = opt$k, iter = 10000)
+  p.out.clu<-ggbiplot(pca.outlier.free, var.axes = F, labels = lab, groups = as.factor(clusters$cluster))
 
-cluster_result<-data.table(sample_id = 1:nrow(outlier.free), cluster_id = clusters$cluster)
+  cluster_result<-data.table(sample_id = 1:nrow(outlier.free), cluster_id = clusters$cluster)
 
-fwrite(cluster_result,
-       file = file.path(opt$o, paste0(basename(dirname(opt$f)), '_clusters.tsv')), 
+  fwrite(cluster_result,
+       file = file.path(opt$o, paste0(basename(dirname(opt$f)),'_', i, '_clusters.tsv')), 
        col.names = F, row.names = F, quote = F, sep = '\t')
 
-file.name.out<-paste0(basename(dirname(opt$f)),'_outlier_free_with_cluster', '.pdf')
-ggsave(p.out.clu, filename = file.path(opt$o, file.name.out))
-       
+  file.name.out<-paste0(basename(dirname(opt$f)),'_', i,'_outlier_free_with_cluster', '.pdf')
+  ggsave(p.out.clu, filename = file.path(opt$o, file.name.out))
+}
+
+cluster_split(2)
+cluster_split(3)
