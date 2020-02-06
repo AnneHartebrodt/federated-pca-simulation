fadaaca8ed75b15b3324f448fbdfd5b71662f09b
Anne Hartebrodt <hartebrodt@imada.sdu.dk>
2020-02-06 10:52:09 +0100

    small changes

--- R/PCA_bor_aor_plot.R
+++ R/PCA_bor_aor_plot.R
@@ -11,7 +11,7 @@ option_list = list(
               help="coding genes output file"),
   make_option(c("-o", "--output.folder"), action="store", default=NA, type='character',
               help="The output file"),
-  make_option(c("-k", "--number.clusters"), action="store", default=NA, type='character',
+  make_option(c("-k", "--number.clusters"), action="store", default=NA, type='numeric',
               help="The number of clusters for cluster detection")
 )
 opt = parse_args(OptionParser(option_list=option_list))
