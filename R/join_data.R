a015078b49ac90cc62470c01f1513fe56c024137
Anne Hartebrodt <anne.hartebrodt@tum.de>
2020-02-07 16:28:06 +0100

    added command line param

--- R/join_data.R
+++ R/join_data.R
@@ -3,20 +3,26 @@ require(optparse)
 
 option_list = list(
   make_option(c("-d", "--dir"), action="store", default=NA, type='character',
-              help="coding genes output file")
+              help="coding genes output file"),
+  make_option(c("-o", "--out"), action="store", default=NA, type='character',
+              help="outdir")
 )
 opt = parse_args(OptionParser(option_list=option_list))
 
-dirs<- list.dirs(opt$d, recursive = F)
 
+#
+opt$d<-'/home/anne/Documents/featurecloud/data/tcga/data_clean'
+opt$o<-'/home/anne/Documents/featurecloud/data/tcga/data_fake'
+dirs<- list.dirs(opt$d, recursive = F)
 for(d in 1: length(dirs)){
   nr_lines<-nrow(fread(file.path(dirs[d],'coding_only.tsv')))
-  for(dd in d:length(dirs)){
+  for(dd in (d+1):length(dirs)){
     nr_lines.2<-nrow(fread(file.path(dirs[dd],'coding_only.tsv')))
-    print(paste0("cat ", dirs[d], '/coding_only.tsv', ' > ', basename(dirs[d]), '_', basename(dirs[dd]), '.tsv'))
-    print(paste0("cat ", dirs[dd], '/coding_only.tsv', ' >> ', basename(dirs[d]), '_', basename(dirs[dd]), '.tsv'))
-    dd<-data.table(seq(1:(nr_lines+nr_lines.2)), c(rep(1, nr_lines), rep(2, nr_lines.2)))
-    fwrite(dd, file = paste0(basename(dirs[d]), '_', basename(dirs[dd]), 'clusters.tsv'), sep='\t')
+    print(paste0("cat ", dirs[d], '/coding_only.tsv', ' > ', paste0(opt$o,'/', basename(dirs[d]), '_', basename(dirs[dd]), '.tsv')))
+    print(paste0("cat ", dirs[dd], '/coding_only.tsv', ' >> ', paste0(opt$o, '/', basename(dirs[d]), '_', basename(dirs[dd]), '.tsv')))
+    print(paste0(basename(dirs[d]), '_', basename(dirs[dd]), 'clusters.tsv'))
+    da<-data.table(seq(1:(nr_lines+nr_lines.2)), c(rep(1, nr_lines), rep(2, nr_lines.2)))
+   # fwrite(da, file = paste0(basename(dirs[d]), '_', basename(dirs[dd]), 'clusters.tsv'), sep='\t')
   }
 }
 
