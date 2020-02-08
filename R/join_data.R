28545dc25d3f4745a53dcd1052f79f77f6ce9840
Anne Hartebrodt <anne.hartebrodt@tum.de>
2020-02-08 09:13:55 +0100

    removed header of second file

--- R/join_data.R
+++ R/join_data.R
@@ -15,11 +15,11 @@ opt$d<-'/home/anne/Documents/featurecloud/data/tcga/data_clean'
 opt$o<-'/home/anne/Documents/featurecloud/data/tcga/data_fake'
 dirs<- list.dirs(opt$d, recursive = F)
 for(d in 1: length(dirs)){
-  nr_lines<-nrow(fread(file.path(dirs[d],'coding_only.tsv')))
+  nr_lines<-nrow(fread(file.path(dirs[d],'coding_only.tsv'), header = T))
   for(dd in (d+1):length(dirs)){
-    nr_lines.2<-nrow(fread(file.path(dirs[dd],'coding_only.tsv')))
+    nr_lines.2<-nrow(fread(file.path(dirs[dd],'coding_only.tsv'), header = T))
     print(paste0("cat ", dirs[d], '/coding_only.tsv', ' > ', paste0(opt$o,'/', basename(dirs[d]), '_', basename(dirs[dd]), '.tsv')))
-    print(paste0("cat ", dirs[dd], '/coding_only.tsv', ' >> ', paste0(opt$o, '/', basename(dirs[d]), '_', basename(dirs[dd]), '.tsv')))
+    print(paste0("tail -n ", nr_lines.2,' ', dirs[dd], '/coding_only.tsv', ' >> ', paste0(opt$o, '/', basename(dirs[d]), '_', basename(dirs[dd]), '.tsv')))
     print(paste0(basename(dirs[d]), '_', basename(dirs[dd]), 'clusters.tsv'))
     da<-data.table(seq(1:(nr_lines+nr_lines.2)), c(rep(1, nr_lines), rep(2, nr_lines.2)))
    # fwrite(da, file = paste0(basename(dirs[d]), '_', basename(dirs[dd]), 'clusters.tsv'), sep='\t')
