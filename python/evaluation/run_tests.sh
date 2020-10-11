852e7df7000ad7550d9daddbbfa984eaf4c016ab
Anne Hartebrodt <hartebrodt@imada.sdu.dk>
2020-10-11 16:01:57 +0200

    minor changes and fixes

--- python/evaluation/run_tests.sh
+++ python/evaluation/run_tests.sh
@@ -18,12 +18,16 @@
 #--filetype 'delim' --center -o '/home/anne/Documents/featurecloud/pca/vertical-pca/results/MMRF-COMMPASS' -r 20 -k 10 \
 # -i 2000 --sep '\t' --header 0 --rownames 0 --names 'MMRF-COMMPASS' --scale
 
-gwaspath='/home/anne/Documents/featurecloud/pca/vertical-pca/'
+
+gwaspath=$1
+export PYTHONPATH=$PYTHONPATH:$gwaspath/federated_dp_pca
+echo $PYTHONPATH
+conda activate federated-pca
 datapath=$gwaspath/data/1000g/raw
 resultpath=$gwaspath/results/1000g
 for e in {1..22} ; do
-python3 /home/anne/Documents/featurecloud/pca/federated_dp_pca/python/PCA/guo_vertical_runner_benchmark_edition.py -f \
+python3 $gwaspath/federated_dp_pca/python/PCA/guo_vertical_runner_benchmark_edition.py -f \
 $datapath/chr${e}/chr${e}.thin \
 --filetype 'gwas' --center -o $resultpath/chr${e} -r 10 -k 10 \
- -i 2000 --sep '\t' --header 0 --rownames 0 --names chr${e} --scale --compare_pca $resultpath/chr${e}/chr${e}.thin.eigenvec.values
+ -i 2000 --sep '\t' --header 0 --rownames 0 --names chr${e} --scale --compare_pca $resultpath/chr${e}/plink/chr${e}.thin.eigenvec.values
 done
