b49d17a8d5bcc0f6c7b798b127e7b3119dc8128f
Anne Hartebrodt <hartebrodt@imada.sdu.dk>
2021-12-08 12:51:57 +0100

    scalability test

--- misc_scipts/run_scalability_test.sh
+++ misc_scipts/run_scalability_test.sh
@@ -7,7 +7,7 @@ echo $PYTHONPATH
 outdir=$basepath'/results/scalability'
 mkdir -p $outdir
 indir=$basepath'/data/mnist/raw'
-#python3 $scriptpath/python/PCA/vertical/scalability.py -f $indir -o $outdir
+python3 $scriptpath/python/PCA/vertical/scalability.py -f $indir -o $outdir
 
 cd $outdir
 echo $scriptpath/R/vertical-pca-benchmark/data_cleanup/scalability_aggregation.R
